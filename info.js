f4ac27c3583f3261032dcd1e638a3d2a28c8a3d3
ashishkumar3 <ashishklingwal@gmail.com>
2018-07-27 18:21:59 +0530

    Added GO class and loaded image using Promises

--- info.js
+++ info.js
@@ -1,164 +1,123 @@
+import GameObject from './GameObject.js';
 
 
+const windowWidth = window.innerWidth;
+const windowHeight = window.innerHeight;
 
-const canvas = document.getElementById('screen');
+
+// Creating canvas amd context.
+const canvas = document.createElement('canvas');
+canvas.style.border = "2px solid black";
+canvas.width = windowWidth;
+canvas.height = windowHeight;
+let body = document.getElementsByTagName("body")[0];
+body.appendChild(canvas);
 const context = canvas.getContext('2d');
-var collection = [];
-
 
 //MOVEMENT PHYSICS AND ANIMATION
-document.addEventListener("keydown",function(e){
-console.log(e.which);
-if (e.keyCode == 68) { //MOVE RIGHT
-current_anim = 1;
-x+=1;
-}
+document.addEventListener("keydown", function(e) {
+  console.log(e.which);
+  if (e.keyCode == 68) { //MOVE RIGHT
+    current_anim = 1;
+    x += 1;
+  }
 
-if (e.keyCode == 87) { //MOVE UP
-current_anim = 3;
-y-=1;
-}
-if (e.keyCode == 65) { //MOVE LEFT
-current_anim = 2;
-x-=1;
-}
-if (e.keyCode == 83) { //MOVE UP
-current_anim = 4;
-y+=1;
-}
+  if (e.keyCode == 87) { //MOVE UP
+    current_anim = 3;
+    y -= 1;
+  }
+  if (e.keyCode == 65) { //MOVE LEFT
+    current_anim = 2;
+    x -= 1;
+  }
+  if (e.keyCode == 83) { //MOVE UP
+    current_anim = 4;
+    y += 1;
+  }
 });
 
-document.addEventListener("keyup",function(e){
-current_anim = 0;
+document.addEventListener("keyup", function(e) {
+  current_anim = 0;
 });
 
-document.addEventListener("DOMContentLoaded",function(){
-Start();
+document.addEventListener("load", function() {
+  Start();
 });
 
+// Draw background using Promise.
 
-function lerp(a, b, t) {
-    var len = a;
-    if(b != len) return;
+function loadImage(path) {
+  return new Promise((resolve) => {
+    let image = new Image();
+    image.src = path;
+    image.addEventListener('load', function() {
+      // for(let i=0; i<100; i++)
+      resolve(image);
+    });
 
-    var x =0;
-  //  x = x + ()
-        x.push(a[i] + t * (b[i] - a[i]));
-    return x;
+  });
 }
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
 // SIRF ANIMATION
 
 
-var spritesheet = new Image();  // LOAD HUA SHEEY
+let spritesheet = new Image(); // LOAD HUA SHEEY
 spritesheet.src = "player_sheet.png";
-var bg = new Image();
-bg.src = "bg.png";
-var OBJ_A = new Image();
+
+
+
+let OBJ_A = new Image();
 OBJ_A.src = "tree_A.png";
-var current_anim = 0; // ANIMATION PLAY  0-IDLE   1-WALK RIGHT   2-WALK LEFT 3-WALK UP 4-WALK DOWN
-var anim_data_column = new Array(1,9,9,9,9);
-var anim_data_row = new Array(10,11,9,8,10);
+let current_anim = 0; // ANIMATION PLAY  0-IDLE   1-WALK RIGHT   2-WALK LEFT 3-WALK UP 4-WALK DOWN
+let anim_data_column = new Array(1, 9, 9, 9, 9);
+let anim_data_row = new Array(10, 11, 9, 8, 10);
 
-var x = 50;
-var y = 50;
-var srcX;
-var srcY ;
+let x = 50;
+let y = 50;
+let srcX;
+let srcY;
 
-var col = 0;
-var row=0;
-var width = 64;
-var height =64;
-var currentframe = 0;
-
-
-var posx = [];
-var posy = [];
-var i;
-
-// function assign_pos(){
-// for ( i = 0 ; i <40 ; i ++){
-//   posx[i] =(Math.random() * 2000) + 0;
-//   posy[i] = (Math.random() * 1000)+84;
-//   console.log(   posx[i] );
-//
-// }
-// }
+let col = 0;
+let row = 0;
+let width = 64;
+let height = 64;
+let currentframe = 0;
 
 
-//
-// function updateFrame(){
-//   srcX = currentframe * width;
-//   srcY = height*anim_data_row[current_anim] ;
-// //  currentframe = ++ currentframe% anim_data_column[current_anim];
-// currentframe ++;
-// if ( currentframe >=anim_data_column[current_anim] )
-//       currentframe = 0;
-//       context.clearRect(0,0,800,500);
-//
-// //context.clearRect(x,y,width,height);
-//
-// }
-
-// function drawImage(){
-//   updateFrame();
-// //  context.translate(-x,-y);
-//   context.drawImage(bg,0,0,3000,3000,-x,-y,3000,3000);
-// for ( i = 0 ; i <40 ; i ++)
-//  context.drawImage(OBJ_A, 0,0,84,145,-x+posx[i],-y+posy[i],84,145);
-//   context.drawImage(spritesheet,srcX,srcY,width,height,400,250,width,height);
-// }
-
-setInterval(function(){
-  context.save();
-  context.translate(10,10);
-    context.restore();
-  console.log("wasaa");
-  //drawImage();
-},100);
+let posx = [];
+let posy = [];
+let i;
 
 
-function Start(){
- context.drawImage(bg,0,0,3000,3000); //DRAW BACKGROUND
 
-for (i =0 ; i <100;i ++)
-new GameObject("tree",OBJ_A,(Math.random() * 1000),(Math.random() * 1000));
+function updateFrame() {
+  srcX = currentframe * width;
+  srcY = height * anim_data_row[current_anim];
+  currentframe = ++currentframe % anim_data_column[current_anim];
+  currentframe++;
+  if (currentframe >= anim_data_column[current_anim])
+    currentframe = 0;
+  // context.clearRect(0, 0, windowWidth, windowHeight); //KYU DALA YE
+
+  // context.clearRect(x,y,width,height);
+  loadImage('bg.png').then(image => {
+    console.log('heloo');
+    context.drawImage(image, 0, 0); //DRAW BACKGROUND
+    console.log(image);
+  });
+  context.drawImage(spritesheet, srcX, srcY, width, height, 400, 250, width, height);
+  requestAnimationFrame(updateFrame); // Always use
 
 }
 
+updateFrame();
 
+function Start() {
 
-
-
-
-
-
-
-function GameObject (name,sprite,xpos,ypos){
-    this.name = name;
-    this.sprite = sprite;
-    this.xpos = xpos;
-    this.ypos = ypos;
-
-    this.make =function(){
-        context.drawImage(this.sprite,this.xpos,this.ypos);
-        collection[collection.length+1] = this.GameObject;
-        this.id = collection.length;
-      }
-
- this.make();
+  console.log('asodnaisnd')
+  for (i = 0; i < 100; i++) {
+    // let tree = new GameObject("tree", OBJ_A, (Math.random() * 1000), (Math.random() * 1000)).make(context);
+  }
 }
